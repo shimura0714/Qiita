e43107a961836a8734f0bfdb7089837a74756897
Atsushi Shimura <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2018-03-16 19:39:37 +0900

    PostgresSQL接続用テスト

--- postgresql.py
+++ postgresql.py
@@ -2,11 +2,16 @@ import psycopg2
 
 
 def get_connection():
-  return psycopg2.connect('postgresql://######:@127.0.0.1:5432/python-dev')
+  return psycopg2.connect('postgresql://#####:@127.0.0.1:5432/python-dev')
 
 conn = get_connection()
 cur  = conn.cursor()
-cur.execute("insert into exmaple (id, name) values('0001', '########')")
+cur.execute("insert into exmaple (id, name) values('0005', 'test01')")
 conn.commit()
+cur.execute("select * from exmaple");
+
+for row in cur:
+  print(row)
+
 cur.close()
 conn.close()
